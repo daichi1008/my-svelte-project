c44059f6f5888c73ddded748f694cc1fa7cc5e0f
daichi1008 <forester.dn1008@gmail.com>
2022-01-10 22:41:52 +0900

    0110

---
+++ my-svelte-project/src/App.svelte
@@ -0,0 +1,44 @@
+<script>
+	import ToDoInputForm from "./ToDoInputForm.svelte";
+    import ToDoList from "./ToDoList.svelte";
+	let nameEnterde=false
+
+	let lastName=""
+	let firstName=""
+
+$:fullName= lastName +" " +firstName
+
+function handleSubmit(){
+	if (lastName && firstName){
+		nameEnterde=true
+	}
+}
+</script>
+
+<main>
+	{#if nameEnterde}
+	<h1>タスクリストアプリケーションへようこそ{fullName}さん!</h1>
+	<ToDoInputForm userName={fullName}/>
+	<ToDoList/>
+	{:else}
+	<h1>タスクリストアプリケーションへようこそ!</h1>
+	<h3>名前を入力してください。</h3>
+	<form on:submit|preventDefault={handleSubmit}>
+	<div>
+		<input bind:value={lastName} type="text" placeholder="姓" required>
+	</div>
+	<div>
+		<input bind:value={firstName} type="text" placeholder="名" required>
+	</div>
+	<button type="submit">タスク管理を始める</button>
+	</form>
+	{/if}
+</main>
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 450px;
+		margin: 0 auto;
+	}
+</style>
